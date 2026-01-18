17f637e2d25ad027f86d97c581cbbc196a7acc19
Bill Lange <billlange1968@yahoo.com>
2026-01-18 10:55:05 -0500

    Update test.py

--- CIS554/Module02/exercise4/test.py
+++ CIS554/Module02/exercise4/test.py
@@ -14,12 +14,55 @@ def test_iso_8601():
     """
     print('Testing iso_8601()')
     
-    result = func.iso_8601('09:30:15')
+    s='09:30:15'
+    print('Testing: '+s)
+    result = func.iso_8601(s)
     introcs.assert_equals(True,result)
 
-    result = func.iso_8601('99:99:99')
+    s='99:99:99'
+    print('Testing: '+s)
+    result = func.iso_8601(s)
     introcs.assert_equals(False,result)
 
+    s='12:5:05'
+    print('Testing: '+s)
+    result = func.iso_8601(s)
+    introcs.assert_equals(False,result)
+
+    s=('12:05:5')
+    print('Testing: '+s)
+    result = func.iso_8601(s)
+    introcs.assert_equals(False,result)
+
+    s='2:05:05'
+    print('Testing: '+s)
+    result = func.iso_8601(s)
+    introcs.assert_equals(True,result)
+
+    s='-2:05:05'
+    print('Testing: '+s)
+    result = func.iso_8601(s)
+    introcs.assert_equals(False,result)
+
+    s='02:-5:05'
+    print('Testing: '+s)
+    result = func.iso_8601(s)
+    introcs.assert_equals(False,result)
+
+    s='02:15:-5'
+    print('Testing: '+s)
+    result = func.iso_8601(s)
+    introcs.assert_equals(False,result)
+
+    s='12:15'
+    print('Testing: '+s)
+    result = func.iso_8601(s)
+    introcs.assert_equals(False,result)
+
+    s='12:15:99'
+    print('Testing: '+s)
+    result = func.iso_8601(s)
+    introcs.assert_equals(False,result)
 
 if __name__ == '__main__':
     test_iso_8601()
