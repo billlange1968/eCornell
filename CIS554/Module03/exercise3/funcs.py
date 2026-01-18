32b6fdcec159f3f2315ab4a2de5e6a4df82f8e40
Bill Lange <billlange1968@yahoo.com>
2026-01-18 16:04:20 -0500

    Update funcs.py

--- CIS554/Module03/exercise3/funcs.py
+++ CIS554/Module03/exercise3/funcs.py
@@ -20,7 +20,17 @@ def factorial(n):
     Parameter n: The integer for the factorial
     Precondition: n is an int >= 0
     """
-    pass
+
+    a=1
+
+    if n<0:
+        return 
+    else:
+        for i in range(1, n+1):
+            a *=i
+
+    return a
+
 
 
 def revrange(a,b):
