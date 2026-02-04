16c11dba5e3dd2f51bf16dd21001ba0d0159851b
Bill Lange <billlange1968@yahoo.com>
2026-02-04 18:36:00 -0500

    Update funcs.py

--- MasteringDataStructures/Module01/Exercise02/funcs.py
+++ MasteringDataStructures/Module01/Exercise02/funcs.py
@@ -26,4 +26,35 @@ def add_time1(time1, time2):
     Parameter time2: the time to add
     Precondition: time2 is a Time object
     """
-    pass
+    hours = time1.hours+time2.hours
+    minutes = time1.minutes+time2.minutes
+    if minutes>60:
+      hours=hours+1
+      minutes=minutes-60
+    time3=clock.Time(hours,minutes)
+    return time3
+
+
+def add_time2(time1, time2):
+    """
+    Modifies time1 to be the sum of time1 and time2
+    
+    DO NOT RETURN a new time object.  Modify the object time1 instead.
+    
+    Examples: 
+        The sum of 12hr 13min and 13hr 12min is 25hr 25min 
+        The sum of 1hr 59min and 3hr 2min is 5hr 1min 
+    
+    Parameter time1: the starting time
+    Precondition: time1 is a Time object
+    
+    Parameter time2: the time to add
+    Precondition: time2 is a Time object
+    """
+    hours = time1.hours+time2.hours
+    minutes = time1.minutes+time2.minutes
+    if minutes>60:
+      hours=hours+1
+      minutes=minutes-60
+    time1.hours=hours
+    time1.minutes=minutes
