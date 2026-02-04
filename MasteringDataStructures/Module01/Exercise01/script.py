e53317cdc3356816bbec1bddd2ab9078a0a1e588
Bill Lange <billlange1968@yahoo.com>
2026-02-04 17:14:50 -0500

    Create script.py

---
+++ MasteringDataStructures/Module01/Exercise01/script.py
@@ -0,0 +1,14 @@
+"""
+A script to show off the creation and use of objects.
+
+Author: William R. Lange
+Date: 2026-02-04
+"""
+import introcs
+import funcs
+
+
+# Step 1: Create a green RGB object, assign it to variable green, and then print it
+# Step 2: Create a 50% transparent red RGB object, assign it to variable red, and print it
+# Step 3: Call the function blend on red/green, assign it to variable brown, and print it
+# Step 4: Call the function blendUnder on green/red, and then print variable green
