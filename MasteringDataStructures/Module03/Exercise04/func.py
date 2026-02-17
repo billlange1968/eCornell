ed9f2225d35783302b6bbaa1e314da0f153278fe
Bill Lange <billlange1968@yahoo.com>
2026-02-16 21:14:12 -0500

    Implement avg function with docstring
    Added a docstring to the avg function explaining its purpose and usage.

---
+++ MasteringDataStructures/Module03/Exercise04/func.py
@@ -0,0 +1,23 @@
+"""
+Module to demonstrate tuple expansion.
+
+Author: William R. Lange
+Date: 2026-02-16
+"""
+
+
+def avg( ):   # The parameter is MISSING.  Add it back. 
+    """
+    Returns average of all of arguments (passed via tuple expansion)
+    
+    Remember that the average of a list of arguments is the sum of all of the elements 
+    divided by the number of elements.
+    
+    Examples: 
+        avg(1.0, 2.0, 3.0) returns 2.0
+        avg(1.0, 1.0, 3.0, 5.0) returns 2.5
+    
+    Parameter args: the function arguments
+    Precondition: args are all numbers (int or float)
+    """
+    pass
