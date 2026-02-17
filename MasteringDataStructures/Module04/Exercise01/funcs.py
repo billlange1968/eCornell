620a597fba87fe9142c3d603ea9b42d42f28463f
Bill Lange <billlange1968@yahoo.com>
2026-02-17 14:49:47 -0500

    Update funcs.py

--- MasteringDataStructures/Module04/Exercise01/funcs.py
+++ MasteringDataStructures/Module04/Exercise01/funcs.py
@@ -1,52 +1,24 @@
 """
-Module demonstrating immutable functions on nested lists.
+Module demonstrating mutable functions on nested lists.
 
-All of these functions make use of accumulators that make new lists.
+All of these functions modify their list arguments.
 
 Author: William R. Lange
 Date: 2026-02-17
 """
 
 
-def row_sums(table):
-    """
-    Returns a list that is the sum of each row in a table.
-    
-    This function assumes that table has no header, so each row has only numbers in it.
-    
-    Examples: 
-        row_sums([[0.1,0.3,0.5],[0.6,0.2,0.7],[0.5,1.1,0.1]]) returns [0.8, 1.5, 1.7]
-        row_sums([[0.2,0.1],[-0.2,0.1],[0.2,-0.1],[-0.2,-0.1]]) returns [0.3, -0.1, 0.1, -0.3]
-    
-    Parameter table: the nested list to process
-    Precondition: table is a table of numbers.  In other words, 
-        (1) table is a nested 2D list in row-major order, 
-        (2) each row contains only numbers, and 
-        (3) each row is the same length.
-    """
-    numrows = len(table)
-    numcols = len(table[0])
-    result = []
-    for r in range(numrows):
-        sum = 0
-        for c in range(numcols):
-            value = table[r][c]
-            sum = sum + value
-        result.append(sum)
-    return result
-
-
 def crossout(table,row,col):
     """
-    Returns a copy of the table, missing the given row and column.
-      
-    Examples:
-        crossout([[1,3,5],[6,2,7],[5,8,4]],1,2) returns [[1,3],[5,8]]
-        crossout([[1,3,5],[6,2,7],[5,8,4]],0,0) returns [[2,7],[8,4]]
-        crossout([[1,3],[6,2]],0,0) returns [[2]]
-        crossout([[6]],0,0) returns []
+    Modifies the table to remove the given row and column.
     
-    Parameter table: the nested list to process
+    Examples:
+        If a = [[1,3,5],[6,2,7],[5,8,4]], crossout(a,1,2) changes a to [[1,3],[5,8]]
+        If a = [[1,3,5],[6,2,7],[5,8,4]], crossout(a,0,0) changes a to [[2,7],[8,4]]
+        If a = [[1,3],[6,2]], crossout(a,0,0) changes a to [[2]]
+        If a = [[6]], crossout(a,0,0) changes a to []
+    
+    Parameter table: the nested list to modify
     Precondition: table is a table of numbers.  In other words, 
         (1) table is a nested 2D list in row-major order, 
         (2) each row contains only numbers, and 
@@ -60,13 +32,49 @@ def crossout(table,row,col):
     """
     numrows = len(table)
     numcols = len(table[0])
-    result = []
-    for r in range(numrows):
-        if r != row:
-            t = []
-            for c in range(numcols):
-                if c != col:
-                    t.append(table[r][c])
-            result.append(t)          
-    return result
 
+    for r in range(numrows):
+        for c in range(numcols):
+            if c == col:
+                del table[r][c]
+                
+    for r in range(numrows):
+        if r == row:
+            del table[r]
+
+
+def place_sums(table):
+    """
+    Modifies the table to add a column summing the previous elements in the row.
+    
+    This function assumes that the table has a header, which means the first row
+    only has strings in it.  The later rows are only numbers.  This function
+    adds the string 'Sum' to the first row.  For each later row, it appends the
+    sum of that row.
+    
+    Example: Suppose that a is
+        
+        [['First','Second','Third'], [0.1,0.3,0.5], [0.6,0.2,0.7], [0.5,1.1,0.1]]
+    
+    then place_sums(a) modifies the table a so that it is now
+        
+        [['First', 'Second', 'Third', 'Sum'],
+         [0.1, 0.3, 0.5, 0.8], [0.6, 0.2, 0.7, 1.5], [0.5, 1.1, 0.1, 1.7]]
+    
+    Parameter table: the nested list to process
+    Precondition: table is a table of numbers with a header.  In other words,
+    (1) table is a nested 2D list in row-major order, (2) the first row only
+    contains strings (the headers) (3) each row after the first contains only
+    numbers, and (4) each row is the same length.
+    """
+    numrows = len(table)
+    numcols = len(table[0])
+
+    table[0].append('Sum')
+    
+    for r in range(1, numrows):
+        sum = 0
+        for c in range(numcols):
+            value = table[r][c]
+            sum = sum + value
+        table[r].append(sum)
