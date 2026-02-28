c3b7add73a8a061138292ea739fbcd3bcbeee375
Bill Lange <billlange1968@yahoo.com>
2026-02-28 14:23:14 -0500

    Create func.py

---
+++ AuditingDatasets/Module01/Exercise02/func.py
@@ -0,0 +1,34 @@
+"""
+Module with a function to read CSV files (converting them into a 2D list)
+
+This function will be used in the main project.  You should hold on to it.
+
+Author: William R. Lange
+Date: 2026-02-28
+"""
+import csv
+
+def read_csv(filename):
+    """
+    Returns the contents read from the CSV file filename.
+    
+    This function reads the contents of the file filename and returns the contents as
+    a 2-dimensional list. Each element of the list is a row, with the first row being
+    the header. Cells in each row are all interpreted as strings; it is up to the 
+    programmer to interpret this data, since CSV files contain no type information.
+    
+    Parameter filename: The file to read
+    Precondition: filename is a string, referring to a file that exists, and that file 
+    is a valid CSV file
+    """
+    result = []
+
+    file = open(filename)
+    wrapper = csv.reader(file)
+ 
+    for row in wrapper:
+        result.append(row)
+
+    file.close()
+
+    return result
