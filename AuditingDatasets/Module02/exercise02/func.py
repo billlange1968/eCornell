271cd67ba60f4ea37b9540db213311cfa8d71948
Bill Lange <billlange1968@yahoo.com>
2026-03-01 20:25:54 -0500

    Create func.py

---
+++ AuditingDatasets/Module02/exercise02/func.py
@@ -0,0 +1,30 @@
+"""
+A simple function comparing datetime objects.
+
+Author: William R. Lange
+Date:   2026-03-01
+"""
+import datetime
+
+
+def is_before(d1,d2):
+    """
+    Returns True if event d1 happens before d2.
+    
+    Values d1 and d2 can EITHER be date objects or datetime objects.
+    If a date object, assume that it happens at midnight of that day. 
+    
+    Parameter d1: The first event
+    Precondition: d1 is EITHER a date object or a datetime object
+    
+    Parameter d2: The first event
+    Precondition: d2 is EITHER a date object or a datetime object
+    """
+    # HINT: Check the type of d1 or d2. If not a datetime, convert it for comparison
+    if type(d1) == datetime.date:
+        d1 = datetime.datetime(d1.year, d1.month, d1.day)
+
+    if type(d2) == datetime.date:
+        d2 = datetime.datetime(d2.year, d2.month, d2.day)
+    
+    return d1<d2
